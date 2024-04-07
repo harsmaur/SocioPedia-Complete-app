543066aa82ef25c177f7d1ef1beea0e6f7cde10c
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-04-07 23:16:04 +0530

    Complete SocialPedia App

---
+++ server/data/index.js
@@ -0,0 +1,134 @@
+import mongoose from "mongoose";
+
+const userIds = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+];
+
+export const users = [
+  {
+    _id: userIds[0],
+    firstName: "test",
+    lastName: "me",
+    email: "aaaaaaa@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p11.jpeg",
+    friends: [],
+    location: "San Fran, CA",
+    occupation: "Software Engineer",
+    viewedProfile: 14561,
+    impressions: 888822,
+    createdAt: 1115211422,
+    updatedAt: 1115211422,
+    __v: 0,
+  },
+  
+ 
+  
+  {
+    _id: userIds[4],
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "janedoe@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p5.jpeg",
+    friends: [],
+    location: "Utah, CA",
+    occupation: "Hacker",
+    viewedProfile: 40212,
+    impressions: 7758,
+    createdAt: 1493463661,
+    updatedAt: 1493463661,
+    __v: 0,
+  },
+  {
+    _id: userIds[5],
+    firstName: "Harvey",
+    lastName: "Dunn",
+    email: "harveydunn@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p7.jpeg",
+    friends: [],
+    location: "Los Angeles, CA",
+    occupation: "Journalist",
+    viewedProfile: 976,
+    impressions: 4658,
+    createdAt: 1381326073,
+    updatedAt: 1381326073,
+    __v: 0,
+  },
+  
+  {
+    _id: userIds[7],
+    firstName: "Jessica",
+    lastName: "Dunn",
+    email: "jessicadunn@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p9.jpeg",
+    friends: [],
+    location: "Washington, DC",
+    occupation: "A Student",
+    viewedProfile: 19420,
+    impressions: 82970,
+    createdAt: 1369908044,
+    updatedAt: 1359322268,
+    __v: 0,
+  },
+];
+
+export const posts = [
+
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[3],
+    firstName: "Whatcha",
+    lastName: "Doing",
+    location: "Korea, CA",
+    description:
+      "Another really long random description. This one is longer than the previous one.",
+    picturePath: "post2.jpeg",
+    userPicturePath: "p6.jpeg",
+    likes: new Map([
+      [userIds[7], true],
+      [userIds[4], true],
+      [userIds[1], true],
+      [userIds[2], true],
+    ]),
+    comments: [
+      "one more random comment",
+      "and another random comment",
+      "no more random comments",
+      "I lied, one more random comment",
+    ],
+  },
+  
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[7],
+    firstName: "Jessica",
+    lastName: "Dunn",
+    location: "Washington, DC",
+    description:
+      "For the last time, I'm going to play video games now. I'm tired of typing. I'm going to play video games now.",
+    picturePath: "post6.jpeg",
+    userPicturePath: "p9.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[2], true],
+    ]),
+
+    comments: [
+      "Can I play video games now?",
+      "No let's actually study",
+      "Never mind, I'm going to play video games",
+      "Stop it.",
+      "Michael, stop it.",
+    ],
+  },
+];
